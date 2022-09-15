9ad8e3d6d1c92b7f6963952412bef4c4c7ceb938
Graham Home <grahamhome333@gmail.com>
2022-09-14 22:44:54 -0400

    Cleanup

--- projects/simple_io/main.py
+++
@@ -1,4 +0,0 @@
-from traffic_control import Intersection
-
-if __name__ == "__main__":
-    Intersection(red_led_pin=2, yellow_led_pin=1, green_led_pin=0, road_sensor_pin=3, crosswalk_sensor_pin=5, buzzer_pin=4, green_led_2_pin=7, yellow_led_2_pin=6).start()
